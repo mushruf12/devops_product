e95607b152152bd0fee0060428cb5e392f41a865
mushruf12 <mushrufs@gmail.com>
2022-10-11 15:01:58 +0530

    Commit

--- Practise/src/main/java/com/CheckPrime.java
+++ Practise/src/main/java/com/CheckPrime.java
@@ -3,6 +3,10 @@ package com;
 public class CheckPrime {
 	 public Boolean prime(int a) {
 			int i=2;
+			if(i<0 || i==1) {
+				System.out.println("Enter number Greater than one");
+				return false;
+			}
 			for(i=2;i<a;i++) {
 				if(a%i==0) {
 					return false;
